fa829a25eed27ca760cdf924ce10105b103912f4
Mark Gwilliam <github@markgwilliam.com>
2016-09-19 08:28:52 +0100

    removed queue name from message queue config

--- src/SFA.DAS.Notifications.Infrastructure/Configuration/AzureServiceBusMessageServiceConfiguration.cs
+++ src/SFA.DAS.Notifications.Infrastructure/Configuration/AzureServiceBusMessageServiceConfiguration.cs
@@ -5,6 +5,5 @@ namespace SFA.DAS.Notifications.Infrastructure.Configuration
     public class AzureServiceBusMessageServiceConfiguration
     {
         public string ConnectionString { get; set; }
-        public string QueueName { get; set; }
     }
 }
