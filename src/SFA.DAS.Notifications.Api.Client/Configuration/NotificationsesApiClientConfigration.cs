120a8697a19ec6288e9380e39deac2bf2ab703ac
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-04-21 10:39:14 +0100

    Add API client and types to project
    Moved the API client and types to the project. Also implemented Azure
    AD authentication.

---
+++ src/SFA.DAS.Notifications.Api.Client/Configuration/NotificationsesApiClientConfigration.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.Notifications.Api.Client.Configuration
+{
+    public class NotificationsApiClientConfiguration : INotificationsApiClientConfiguration
+    {
+        public string ApiBaseUrl { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string IdentifierUri { get; set; }
+        public string Tenant { get; set; }
+    }
+}
