22da231c4ae6fcb4ce75736c7e20feba2e34ec25
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-04-24 09:47:48 +0100

    Add code for retry functionality
    Added the Polly package and also the necessary execution policies to be
    used.

---
+++ src/SFA.DAS.Notifications.Infrastructure/ExecutionPolicies/NoopExecutionPolicy.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Notifications.Infrastructure.ExecutionPolicies
+{
+    public class NoopExecutionPolicy : ExecutionPolicy
+    {
+        public override void Execute(Action action)
+        {
+        }
+        public override async Task ExecuteAsync(Func<Task> action)
+        {
+            await action();
+        }
+
+        public override T Execute<T>(Func<T> func)
+        {
+            return func();
+        }
+        public override async Task<T> ExecuteAsync<T>(Func<Task<T>> func)
+        {
+            return await func();
+        }
+    }
+}
