8d3fe47c5961912c853274c80b0524d434140f7c
Peter Morris <mrpmorris@gmail.com>
2019-02-26 12:08:51 +0000

    Remove property auto-initialiser

--- src/SFA.DAS.Notifications.Domain/Configuration/TemplateConfiguration.cs
+++ src/SFA.DAS.Notifications.Domain/Configuration/TemplateConfiguration.cs
@@ -5,6 +5,6 @@ namespace SFA.DAS.Notifications.Domain.Configuration
     public class TemplateConfiguration
     {
         public List<Template> EmailServiceTemplates { get; set; }
-        public List<SmsTemplate> SmsServiceTemplates { get; set; } = new List<SmsTemplate>();
+        public List<SmsTemplate> SmsServiceTemplates { get; set; }
     }
 }
