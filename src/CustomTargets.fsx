b8dba7ac8fddf9263d37fe411679da99422dad22
chrisfoster186 <chrisfoster186@googlemail.com>
2019-10-11 11:03:10 +0100

    Adds restore for messages proj to custom targets

--- src/CustomTargets.fsx
+++ src/CustomTargets.fsx
@@ -13,4 +13,7 @@ Target "Dotnet Restore" (fun _ ->
     DotNetCli.Restore(fun p ->
         { p with
                 Project = ".\\SFA.DAS.Notifications.Api.Types" })
+    DotNetCli.Restore(fun p ->
+        { p with
+                Project = ".\\SFA.DAS.Notifications.Messages" })
 )
