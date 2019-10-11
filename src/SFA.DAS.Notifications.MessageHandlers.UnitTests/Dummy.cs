f004ad903db5b7c10c43bb8d2aafa71aafe705cd
chrisfoster186 <chrisfoster186@googlemail.com>
2019-10-11 11:20:26 +0100

    Adds dummy test

---
+++ src/SFA.DAS.Notifications.MessageHandlers.UnitTests/Dummy.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.Notifications.MessageHandlers.UnitTests
+{
+    [TestFixture]
+    public class Dummy
+    {
+        [Test]
+        public void Pass()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
