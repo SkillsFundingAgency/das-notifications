cd773f7593a7ac49983061850389944479ce214e
chrisfoster186 <chrisfoster186@googlemail.com>
2019-10-15 15:14:31 +0100

    Adds handler and test (WIP - unit test fails as not implemented)

---
+++ src/SFA.DAS.Notifications.MessageHandlers.UnitTests/CommandHandlers/SendEmailCommandHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Moq;
+using NServiceBus;
+using NUnit.Framework;
+using SFA.DAS.Notifications.Application.Commands.SendEmail;
+using SendEmailCommandHandler = SFA.DAS.Notifications.MessageHandlers.CommandHandlers.SendEmailCommandHandler;
+
+namespace SFA.DAS.Notifications.MessageHandlers.UnitTests.CommandHandlers
+{
+    [TestFixture]
+    public class SendEmailCommandHandlerTests
+    {
+        private SendEmailCommandHandler _handler;
+        private Mock<IMediator> _mediator;
+        private SendEmailCommand _message;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _mediator = new Mock<IMediator>();
+
+            _mediator.Setup(x => x.SendAsync(It.IsAny<SendEmailCommand>()));
+
+            _handler = new SendEmailCommandHandler(_mediator.Object);
+
+            _message = new SendEmailCommand();
+        }
+
+        [Test]
+        public async Task Handle_Should_Send_Email()
+        {
+            await _handler.Handle(_message, Mock.Of<IMessageHandlerContext>());
+
+            _mediator.Verify(x => x.SendAsync(It.Is<SendEmailCommand>(c => c.TemplateId == _message.TemplateId && c.RecipientsAddress == _message.RecipientsAddress)));
+        }
+    }
+}
---
+++ src/SFA.DAS.Notifications.MessageHandlers/CommandHandlers/SendEmailCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using NServiceBus;
+using SFA.DAS.Notifications.Application.Commands.SendEmail;
+
+namespace SFA.DAS.Notifications.MessageHandlers.CommandHandlers
+{
+    public class SendEmailCommandHandler : IHandleMessages<SendEmailCommand>
+    {
+        private readonly IMediator _mediator;
+
+        public SendEmailCommandHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Handle(SendEmailCommand message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
