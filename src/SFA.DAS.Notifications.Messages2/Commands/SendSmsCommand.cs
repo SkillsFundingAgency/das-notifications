2741b8491fe2ddc1213e9de5eae7e864a04d64e4
tom-gough <tom@tgsd.co.uk>
2019-11-25 14:54:16 +0000

    Messages project duplicated in .net standard (old duplicates to be removed)

---
+++ src/SFA.DAS.Notifications.Messages2/Commands/SendSmsCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.Notifications.Messages2.Commands
+{
+    public class SendSmsCommand
+    {
+        public string SystemId { get; set; }
+        public string TemplateId { get; set; }
+        public string RecipientsNumber { get; set; }
+        public Dictionary<string,string> Tokens { get; set; }
+
+        public SendSmsCommand(string systemId, string templateId, string recipientsNumber, Dictionary<string,string> tokens)
+        {
+            SystemId = systemId;
+            TemplateId = templateId;
+            RecipientsNumber = recipientsNumber;
+            Tokens = tokens;
+        }
+    }
+}
