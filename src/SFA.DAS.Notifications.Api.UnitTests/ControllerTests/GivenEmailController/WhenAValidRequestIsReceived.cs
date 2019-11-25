7190efa0cff3352906e40a5d3c821ba7e67bcf9e
tom-gough <tom@tgsd.co.uk>
2019-11-25 10:54:40 +0000

    api version uplift

--- src/SFA.DAS.Notifications.Api.UnitTests/ControllerTests/GivenEmailController/WhenAValidRequestIsReceived.cs
+++ src/SFA.DAS.Notifications.Api.UnitTests/ControllerTests/GivenEmailController/WhenAValidRequestIsReceived.cs
@@ -1,12 +1,12 @@
-﻿using Moq;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.Notifications.Api.Controllers;
 using SFA.DAS.Notifications.Api.Core;
 using SFA.DAS.Notifications.Api.Orchestrators;
 using SFA.DAS.Notifications.Api.Types;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.Notifications.Api.UnitTests.ControllerTests.GivenEmailController
 {
@@ -17,11 +17,11 @@ namespace SFA.DAS.Notifications.Api.UnitTests.ControllerTests.GivenEmailControll
         {
             var orchestratorResponse = new OrchestratorResponse { Code = NotificationOrchestratorCodes.Post.Success };
             var mockNotificationOrchestrator = new Mock<INotificationOrchestrator>();
-            mockNotificationOrchestrator.Setup(x => x.SendEmail(It.IsAny<Email>())).Returns(Task.FromResult(orchestratorResponse));
+            mockNotificationOrchestrator.Setup(x => x.SendSms(It.IsAny<Sms>())).Returns(Task.FromResult(orchestratorResponse));
 
-            var sut = new EmailController(mockNotificationOrchestrator.Object);
+            var sut = new SmsController(mockNotificationOrchestrator.Object);
 
-            var request = new Email();
+            var request = new Sms();
             HttpResponseMessage controllerResponse = await sut.Post(request);
 
             Assert.AreEqual(HttpStatusCode.OK, controllerResponse.StatusCode);
--- src/SFA.DAS.Notifications.Api.UnitTests/ControllerTests/GivenEmailController/WhenAnInvalidRequestIsReceived.cs
+++ src/SFA.DAS.Notifications.Api.UnitTests/ControllerTests/GivenEmailController/WhenAnInvalidRequestIsReceived.cs
@@ -1,12 +1,12 @@
-﻿using Moq;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.Notifications.Api.Controllers;
 using SFA.DAS.Notifications.Api.Core;
 using SFA.DAS.Notifications.Api.Orchestrators;
 using SFA.DAS.Notifications.Api.Types;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.Notifications.Api.UnitTests.ControllerTests.GivenEmailController
 {
