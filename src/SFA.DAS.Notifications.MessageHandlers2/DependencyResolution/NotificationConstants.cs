345e111fa7c7b24fa89e96f9f225792c17a8428e
tom-gough <tom@tgsd.co.uk>
2019-12-05 08:52:26 +0000

    removed old message handlers projects

--- src/SFA.DAS.Notifications.MessageHandlers2/DependencyResolution/NotificationConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SFA.DAS.Notifications.MessageHandlers2.DependencyResolution
-{
-    public static class NotificationConstants
-    {
-        public const string ServiceName = "SFA.DAS.Notifications";
-        public const string Version = "1.0";
-    }
-}
