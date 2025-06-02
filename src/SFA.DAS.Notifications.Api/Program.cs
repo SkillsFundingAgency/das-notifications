e8908e4c599834cc09629bd9e874eba28f84dfce
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-06-02 16:02:45 +0100

    P2-1956-Kibana decommisioning

--- src/SFA.DAS.Notifications.Api/Program.cs
+++ src/SFA.DAS.Notifications.Api/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using SFA.DAS.NServiceBus.Configuration.MicrosoftDependencyInjection;
-using NLog.Web;
-using SFA.DAS.Configuration.AzureTableStorage;
 
 namespace SFA.DAS.Notifications.Api
 {
@@ -14,29 +10,12 @@ namespace SFA.DAS.Notifications.Api
         public static void Main(string[] args)
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var logger = NLogBuilder.ConfigureNLog(environment == "Development" ? "nlog.Development.config" : "nlog.config").GetCurrentClassLogger();
-
-            try
-            {
-                logger.Info("Starting up host");
-                CreateWebHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                logger.Fatal(ex, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                global::NLog.LogManager.Shutdown();
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseNServiceBusContainer()
-                .UseStartup<Startup>()
-                .UseNLog();
-
+                .UseStartup<Startup>();
     }
 }
